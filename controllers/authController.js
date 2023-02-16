cf6d5eb5d935601e0cef2de86741685a2c20bbe8
MSDoshev <mihaildo@outlook.com>
2023-02-16 15:39:56 +0200

    Add register page

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,4 +4,16 @@ const router = require('express').Router();
 router.get('/login', (req, res)=>{
     res.render('auth/login');
 })
+router.post('/login', (req, res)=>{
+    const {email, password} = req.body;
+
+});
+
+router.get('/register', (req, res) =>{
+    res.render('auth/register');
+});
+router.post('/register', (req, res) =>{
+    const {username, email, password, repeatPassword} = req.body;
+
+});
 module.exports = router;
