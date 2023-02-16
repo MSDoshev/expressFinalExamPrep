fb37560f3b0783e62cd0d1432b064731d5f74d6b
MSDoshev <mihaildo@outlook.com>
2023-02-16 14:31:13 +0200

    Add static files and setup express

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const express = require('express');
+const express = require("express");
+const routes = require("./routes");
 
 const app = express();
 
-app.listen(5000, () => console.log('Server is running on port 5000...'))
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }));
+app.use(routes);
+
+app.listen(5000, () => console.log("Server is running on port 5000..."));
